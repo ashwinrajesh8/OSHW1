d87b44bc1c53cbbdb37c69b8a39620886a409d0c
ashrajesh <ashwinr@smu.edu>
2022-02-09 12:15:45 -0600

    input setup progress

--- hw1.c
+++ hw1.c
@@ -1,5 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
-int main() {
-    printf("Hello World");
+// Function provided to generate random numbers
+int f1(int x, int a, int c, int m) {
+    long int x1 = x * a + c;
+    return (int)(x1 % m);
+}
+
+// Main
+// We assume the input file path to be accessed via argv[1]
+int main(int argc, char **argv) {
+    int numThreads = 3;             // number of threads to be created
+    int numRounds = 5;              // number of rounds to go through
+
+    // Test that file is openable
+//    FILE *fptr;
+//    if ((fptr = fopen(argv[1],"r")) == NULL){
+//        printf("Error! opening file");
+//        exit(1);
+//    }
+
+
+    FILE *in_file = fopen(argv[1], "r");
+    struct stat sb;
+    stat(argv[1], &sb);
+    char *file_contents = malloc(sb.st_size);
+
+    while (fscanf(in_file, "%[^\n] ", file_contents) != EOF) {
+        printf("> %s\n", file_contents);
+    }
+    fclose(in_file);
+
+
+    printf("Got Here! %s", argv[1]);
     return 0;
 }
