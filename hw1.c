2b7bbfa12b892465c1bc36ec7d1c9a273ff8ff83
ashrajesh <ashwinr@smu.edu>
2022-02-09 19:00:39 -0600

    File parsing successful

--- hw1.c
+++ hw1.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include <string.h>
+#include <pthread.h>
 
-struct Thread {
+int temp_a, temp_c, temp_m, temp_x;         // make the temporary ints parsed from each line in file a global var for ez access
+
+struct Thread {                         // each thread is set to have 4 integer properties
     int a;
     int c;
     int m;
@@ -16,56 +18,36 @@ int f1(int x, int a, int c, int m) {
     return (int)(x1 % m);
 }
 
-// Main
 // We assume the input file path to be accessed via argv[1]
 int main(int argc, char **argv) {
     printf("Name of Input File: %s\n", argv[1]);        // "when program starts, you should print out name of file"
 
-    struct Thread *threads;
-
+    struct Thread *threads;                                   // dynamically allocating thread struct cuz we have no idea how many threads exactly there may be
     int numThreads;                                         // number of threads to be created
     int numRounds;                                        // number of rounds to go through
 
-    FILE *in_file = fopen(argv[1], "r");
-    struct stat sb;
-    stat(argv[1], &sb);
-    char *file_contents = malloc(sb.st_size);
-    int counter = 0;
-    while (fscanf(in_file, "%[^\n] ", file_contents) != EOF) {
-        if(counter == 0){numThreads = atoi(file_contents);}
-        else if(counter == 1){numRounds = atoi(file_contents);}
-        else if (counter > 1){
-            printf("Line items: %s\n", file_contents);
-            int temp_a;
-            int temp_c;
-            int temp_m;
-            int temp_x;
-            char *num_store = malloc(sb.st_size);
-            int numCounter = 0;
-//            while (scanf(file_contents, " ", num_store)){
-//                if(numCounter == 0){
-//                    temp_a = atoi(num_store);
-//                }
-//                else if(numCounter == 1){
-//                    temp_c = atoi(num_store);
-//                }
-//                else if(numCounter == 2){
-//                    temp_m = atoi(num_store);
-//                }
-//                else if(numCounter == 3){
-//                    temp_x = atoi(num_store);
-//                }
-//                numCounter++;
-//            }
-            // move below into while loop
-//            for(int i = 0; i < 4; i++){
-//                threads[counter-2].a = temp_a;
-//            }
+    /// File Parsing Starts Here
+        FILE *in_file = fopen(argv[1], "r");
+        struct stat sb;
+        stat(argv[1], &sb);
+        char *file_contents = malloc(sb.st_size);
+        int counter = 0;
+        while (fscanf(in_file, "%[^\n] ", file_contents) != EOF) {
+            if(counter == 0){numThreads = atoi(file_contents);}
+            else if(counter == 1){numRounds = atoi(file_contents);}
+            else if (counter > 1){
+                printf("Line items: %s\n", file_contents);
+                sscanf(file_contents,"%d %d %d %d", &temp_a, &temp_c, &temp_m, &temp_x);
+                threads[counter-2].a = temp_a;
+                threads[counter-2].c = temp_c;
+                threads[counter-2].m = temp_m;
+                threads[counter-2].x = temp_x;
+            }
+            counter++;
         }
-        counter++;
-    }
-    fclose(in_file);
-
+        fclose(in_file);
+    /// File Parsing End
+    // threads, numThreads, and numRounds are populated
 
     printf("Got Here! %s %d %d", argv[1], numThreads, numRounds);
     return 0;
